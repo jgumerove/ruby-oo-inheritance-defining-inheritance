3c32e57971a828a7b0e30e382ed43e3db326e76a
jgumerove <josh.gumerove@gmail.com>
2021-03-02 20:53:09 -0500

    commit tree 915b78e196e0e05f70ea54b6ebfd68ecb3c82985

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+
+#notice that we are requiring relative above --Car class needs access to the vehicle class 
+
+class Car < Vehicle #inherit the car class from the vehicle class with use of <
+
+    def go 
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    def go
+      "vrrrrrrrooom!" #these instance methods describe common vehicle behavior 
+    end
+  
+    def fill_up_tank
+      "filling up!" #see note above 
+    end
 
 end
